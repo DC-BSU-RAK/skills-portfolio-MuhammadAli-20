983bf983a19445ec8dcd8c17b36af456ed1d7070
Muhammad Ali <alioraf789@gmail.com>
2025-11-01 01:29:23 +0400

    Update exercise1.py

--- Assessment 1 - Skills Portfolio/01 - MathsQuiz/exercise1.py
+++ Assessment 1 - Skills Portfolio/01 - MathsQuiz/exercise1.py
@@ -56,11 +56,25 @@ root = Tk()
 
 root.title("Maths Quiz Game")
 
-root.geometry("600x400")
+root.geometry("600x350")
 
 def displayMenu():
     label = Label(root, text = "DIFFICULTY LEVEL MENU", fg = "black", font = ("Arial", 16, "bold"))
     label.pack(pady = 30)
+    button = Button(root, text = "Easy (Single Digits)", command = lambda: randomInt("Easy"), fg = "green", font = ("Arial", 12, "bold"), bg = "#ADD8E6")
+    button.pack(pady = 7)
+    button = Button(root, text = "Moderate (Double Digits)", command = lambda: randomInt("Moderate"), fg = "yellow", font = ("Arial", 12, "bold"), bg = "#ADD8E6")
+    button.pack(pady = 7)
+    button = Button(root, text = "Advanced (Four Digits)", command = lambda: randomInt("Advanced"), fg = "red", font = ("Arial", 12, "bold"), bg = "#ADD8E6")
+    button.pack(pady = 7)
+
+def randomInt(level):
+    if level == "Easy":
+        print(random.randint(1, 9))
+    elif level == "Moderate":
+        print(random.randint(10, 90))
+    else:
+        print(random.randint(1000, 9999))
 
 displayMenu()
 
